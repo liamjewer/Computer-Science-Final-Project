eeec4f79a2e863b045792a67482cad3fc9302bc4
Liam J <liam@jewer.ca>
2019-05-31 11:42:50 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.txtOut = New System.Windows.Forms.TextBox()
+        Me.txtMsg = New System.Windows.Forms.TextBox()
+        Me.txtIP = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(13, 13)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(218, 23)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "listen"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'txtOut
+        '
+        Me.txtOut.Font = New System.Drawing.Font("Monospac821 BT", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOut.Location = New System.Drawing.Point(13, 43)
+        Me.txtOut.Multiline = True
+        Me.txtOut.Name = "txtOut"
+        Me.txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtOut.Size = New System.Drawing.Size(218, 112)
+        Me.txtOut.TabIndex = 1
+        '
+        'txtMsg
+        '
+        Me.txtMsg.AllowDrop = True
+        Me.txtMsg.Location = New System.Drawing.Point(13, 162)
+        Me.txtMsg.Name = "txtMsg"
+        Me.txtMsg.Size = New System.Drawing.Size(218, 20)
+        Me.txtMsg.TabIndex = 2
+        '
+        'txtIP
+        '
+        Me.txtIP.Location = New System.Drawing.Point(131, 188)
+        Me.txtIP.Name = "txtIP"
+        Me.txtIP.Size = New System.Drawing.Size(100, 20)
+        Me.txtIP.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(86, 191)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(20, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "IP:"
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(13, 188)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(53, 23)
+        Me.btnSend.TabIndex = 5
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(243, 219)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtIP)
+        Me.Controls.Add(Me.txtMsg)
+        Me.Controls.Add(Me.txtOut)
+        Me.Controls.Add(Me.btnStart)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents txtOut As TextBox
+    Friend WithEvents txtMsg As TextBox
+    Friend WithEvents txtIP As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnSend As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+Public Class Form1
+    Dim strHostName As String
+    Dim strIPAddress As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        strHostName = System.Net.Dns.GetHostName()
+        strIPAddress = System.Net.Dns.GetHostByName(strHostName).AddressList(0).ToString()
+        Me.Text = strIPAddress
+        txtIP.Text = strIPAddress
+    End Sub
+
+    Sub listen()
+        Dim serverSocket As New TcpListener(15000)
+        Dim requestCount As Integer
+        Dim clientSocket As TcpClient
+        Dim messageReceived As Boolean = False
+        serverSocket.Start()
+        msg("Server Started")
+        clientSocket = serverSocket.AcceptTcpClient()
+        msg("Accept connection from client")
+        requestCount = 0
+
+        While (Not (messageReceived))
+            Try
+                requestCount = requestCount + 1
+                Dim networkStream As NetworkStream =
+                        clientSocket.GetStream()
+                Dim bytesFrom(10024) As Byte
+                networkStream.Read(bytesFrom, 0, bytesFrom.Length)
+                Dim dataFromClient As String =
+                        System.Text.Encoding.ASCII.GetString(bytesFrom)
+                dataFromClient =
+                    dataFromClient.Substring(0, dataFromClient.Length)
+                txtOut.Text += dataFromClient
+                txtOut.Text += vbNewLine
+                messageReceived = True
+                Dim serverResponse As String =
+                    "Server response " + Convert.ToString(requestCount)
+                Dim sendBytes As [Byte]() =
+                    Encoding.ASCII.GetBytes(serverResponse)
+                networkStream.Write(sendBytes, 0, sendBytes.Length)
+                networkStream.Flush()
+                msg(serverResponse)
+            Catch ex As Exception
+                End
+            End Try
+        End While
+
+
+        clientSocket.Close()
+        serverSocket.Stop()
+        msg("exit")
+        Console.ReadLine()
+    End Sub
+
+    Sub msg(ByVal mesg As String)
+        mesg.Trim()
+        Console.WriteLine(" >> " + mesg)
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        listen()
+    End Sub
+
+    Public Sub WriteData(ByVal data As String, ByRef IP As String)
+        Try
+            txtOut.Text += data.PadLeft(30)
+            txtOut.Text += vbNewLine
+            txtMsg.Clear()
+            Console.WriteLine("Sending message """ & data & """ to " & IP)
+            Dim client As TcpClient = New TcpClient()
+            client.Connect(New IPEndPoint(IPAddress.Parse(IP), 1500))
+            Dim stream As NetworkStream = client.GetStream()
+            Dim sendBytes As Byte() = Encoding.ASCII.GetBytes(data)
+            stream.Write(sendBytes, 0, sendBytes.Length)
+        Catch ex As Exception
+            MsgBox("The ip you are trying to contact is not lstening")
+        End Try
+    End Sub
+
+    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+        WriteData(txtMsg.Text, txtIP.Text)
+    End Sub
+End Class
