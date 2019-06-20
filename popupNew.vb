0aa34a903eec234ade2dfdef08f767692b0e8bb5
Liam Jewer <liam@jewer.ca>
2019-06-20 11:49:54 -0400

    random name generator in new convo

--- popupNew.vb
+++ popupNew.vb
@@ -1,8 +1,16 @@
 ﻿Public Class popupNew
     Dim tempIPs As New List(Of String)
+    Dim lines() As String
+    Dim rG As New Random
+
     Private Sub btnNew_Click(sender As Object, e As EventArgs) Handles btnNew.Click
         Form1.newConvo(txtIP.Text, txtPort.Text, txtName.Text)
         Me.Close()
         appMenu.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        lines = Split(System.IO.File.ReadAllText("csv.csv"), vbNewLine)
+        txtName.Text = lines(rG.Next(1, lines.Length))
     End Sub
 End Class
